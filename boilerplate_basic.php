f1dc24327c78266579a32eb3bf55cf89d41de0a2
Timothy Jiang <ttjiang2@illinois.edu>
2013-09-13 19:05:12 -0400

    First commit

---
+++ boilerplate_basic.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title></title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Bootstrap -->
+    <link href="./assets/css/bootstrap.css" rel="stylesheet" media="screen">
+    <link href="./assets/css/docs.css" rel="stylesheet" type="text/css">
+    <link href="./assets/css/misc.css" rel="stylesheet" type="text/css">
+
+  <body>
+
+
+
+    <!-- Placed at the end of the document so the pages load faster -->
+    <!-- JavaScript plugins (requires jQuery referenced first) -->
+    <script src="./assets/js/jquery-1.10.2.js"></script>
+    <script src="./assets/js/bootstrap.js"></script>
+    
+    <!-- Optionally enable responsive features in IE8 -->
+    <script src="./assets/js/respond.js"></script>
+
+    </body>
+</html>
